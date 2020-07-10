fe7d2fd48e73cf4375fddd1fb705812e2832c924
ricardospalves <ricardospalves@gmail.com>
2020-07-10 18:23:06 -0300

    Adicionar formulário de busca

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <h1>APP</h1>
+    <form>
+      <input type="text" v-inputmask="99999-999" autofocus>
+      <button>Buscar endereço</button>
+    </form>
   </div>
 </template>
+
+<script>
+export default {
+  directives: {
+    inputmask: {
+      bind(el, binding, vnode) {
+        new Inputmask('99999-999', {
+          clearMaskOnLostFocus: false
+        }).mask(el)
+      }
+    }
+  }
+}
+</script>
