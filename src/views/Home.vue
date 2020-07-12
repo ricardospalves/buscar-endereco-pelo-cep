f49eb6bd6346f94068db1012c742cefa6c387ed1
ricardospalves <ricardospalves@gmail.com>
2020-07-12 02:40:23 -0300

    Adicionar Vue data e computed relacionados a CEP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,17 +4,41 @@
       <input
         type="text"
         inputmode="numeric"
+        v-model="cep"
         v-inputmask
         autofocus
       >
 
       <button>Buscar endereço</button>
     </form>
+
+    <p>
+      {{ cepRaw }}
+    </p>
+
+    <p>
+      {{ isCepValid }}
+    </p>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      cep: '01001-000'
+    }
+  },
+  computed: {
+    isCepValid() {
+      const regex = /^\d{5}-\d{3}$/
+
+      return regex.test(this.cep)
+    },
+    cepRaw() {
+      return this.cep.trim().replace(/\D+/g, '')
+    }
+  },
   directives: {
     inputmask: {
       bind(el, binding, vnode) {
