0e45f2ee061b0487e3268aa355616cc2969d6dbc
ricardospalves <ricardospalves@gmail.com>
2020-07-15 18:12:03 -0300

    Atualizar os favicons

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,27 @@ module.exports = {
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     manifestOptions: {
-      start_url: 'https://buscarenderecopelocep.netlify.app/'
+      start_url: 'https://buscarenderecopelocep.netlify.app/',
+      icons: [
+        {
+          'src': './img/icons/android-chrome-192x192.png',
+          'sizes': '192x192',
+          'type':
+          'image/png'
+        },
+        {
+          'src': './img/icons/android-chrome-512x512.png',
+          'sizes': '512x512',
+          'type': 'image/png'
+        }
+      ]
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
     },
     workboxOptions: {
       exclude: [
