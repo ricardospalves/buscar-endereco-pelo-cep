995ca1771772b3c72f50bd7dbfb5eef57208befc
ricardospalves <ricardospalves@gmail.com>
2020-07-15 15:57:46 -0300

    Gerar sitemap

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const SiteMap = require('sitemap-webpack-plugin').default
+const URL = 'https://buscarenderecopelocep.netlify.app'
+
 module.exports = {
   pwa: {
     name: 'Buscar endereço pelo CEP',
@@ -12,5 +15,24 @@ module.exports = {
         '_redirects'
       ]
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new SiteMap(
+        URL,
+        [
+          {
+            path: URL,
+            lastmod: new Date().toISOString().replace(/\..+/, ''),
+            priority: '1'
+          }
+        ],
+        {
+          filename: 'sitemap.xml',
+          lastmod: true,
+          priority: '1'
+        }
+      )
+    ]
   }
 }
