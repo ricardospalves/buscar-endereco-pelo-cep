32b184fc2e2a01beffb372d91da7ae663fd1fadd
ricardospalves <ricardospalves@gmail.com>
2020-07-15 11:17:44 -0300

    Adicionar estilos para o componente Address.vue

--- src/components/Address.vue
+++ src/components/Address.vue
@@ -1,13 +1,18 @@
 <template>
-  <label>
+  <label class="block">
     <textarea
-      class="block w-full leading-normal h-6 text-base border border-gray-500"
+      class="block w-full leading-normal h-12 p-2 border-2 border-transparent hover:border-black focus:border-black text-xl text-center text-black focus:outline-none focus:shadow-outline bg-gray-300 hover:bg-transparent focus:bg-transparent rounded-t transition-colors duration-200"
       v-model="value"
       readonly
       @focus="focusHandler"
+      @blur="blurHandler"
     ></textarea>
 
-    <span>Copiar</span>
+    <span
+      class="block py-2 font-bold text-center text-white bg-black hover:bg-gray-800 focus:bg-gray-800 focus:outline-none focus:shadow-outline rounded-b transition-colors duration-200"
+    >
+      {{ isOnClipboard ? 'Copiado' : 'Copiar' }}
+    </span>
   </label>
 </template>
 
@@ -16,6 +21,11 @@ export default {
   props: [
     'value'
   ],
+  data() {
+    return {
+      isOnClipboard: false
+    }
+  },
   methods: {
     focusHandler(event) {
       const elInput = event.currentTarget
@@ -24,6 +34,11 @@ export default {
       elInput.setSelectionRange(0, 99999)
 
       document.execCommand('copy')
+
+      this.isOnClipboard = true
+    },
+    blurHandler() {
+      this.isOnClipboard = false
     }
   },
   mounted() {
